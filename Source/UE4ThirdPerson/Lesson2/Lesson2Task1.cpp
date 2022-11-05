f30299b997f27fd5c4aea5fd60cae87125666e49
studentutuDev <studentutu@gmail.com>
2022-11-05 20:29:13 +0200

    Lesson 2 - added changing mesh via Pure BP, change via Hybrid - CPP/Blueprint

---
+++ Source/UE4ThirdPerson/Lesson2/Lesson2Task1.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Lesson2Task1.h"
+
+
+// Sets default values
+ALesson2Task1::ALesson2Task1()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create default mesh in BP
+	MeshToSet = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	MeshToSet->SetupAttachment(RootComponent);
+
+	// Create default collision in BP
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision"));
+	BoxComponent->SetupAttachment(MeshToSet);
+}
+
+// Called when the game starts or when spawned
+void ALesson2Task1::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ALesson2Task1::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ALesson2Task1::ChangeMesh(AActor* overlappedActor, UStaticMesh* newMesh)
+{
+	if(overlappedActor == GetWorld()->GetFirstPlayerController()->GetPawn())
+	{
+		MeshToSet->SetStaticMesh(newMesh);
+	}
+}
---
+++ Source/UE4ThirdPerson/Lesson2/Lesson2Task1.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "Lesson2Task1.generated.h"
+
+
+/**
+ * @brief
+ * Hybrid approach - cpp checks but get references from BP.
+ */
+UCLASS()
+class UE4THIRDPERSON_API ALesson2Task1 : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UStaticMeshComponent* MeshToSet;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UBoxComponent* BoxComponent;
+
+	
+	// Sets default values for this actor's properties
+	ALesson2Task1();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UFUNCTION(BlueprintCallable)
+	void ChangeMesh(AActor* overlappedActor, UStaticMesh* newMesh);
+};
